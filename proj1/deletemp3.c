08a4dbe9d37287c0c2253d5d560d92c67ae01b61
sam calvert <sfcal@udel.edu>
2021-02-25 16:27:15 -0800

    not working, logic there

--- proj1/deletemp3.c
+++ proj1/deletemp3.c
@@ -4,13 +4,27 @@ extern node_t *head;
 
 void delete(char * name){
 
-node_t *temp;
+node_t *temp = head;
 
-temp=head;
+while(temp!=NULL){ //we always need to traverse full list
 
-while(temp->name != name && temp->next!=NULL){ // this goes to the next mp3
+if(temp->name == name){ //if head deleted
+	head = temp->next;
+	free(temp);
+	continue;
+}
+
+while(temp->name != name && temp!=NULL){ // this goes to the next mp3
+    temp->prev=temp;
     temp=temp->next;
 }
+if(temp->name == name){
+    temp=temp->next;
+    free(temp);
+    continue;
+}
+return;
 printf("temp name is %s",temp->name); // debug
 
+}
 }
