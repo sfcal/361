679eecdba7664e5bdec930676adab6f7155daabb
AlakayCodingCreations <alexjchayes@gmail.com>
2021-02-28 22:13:48 -0500

    Finished code

--- proj1/deletemp3.c
+++ proj1/deletemp3.c
@@ -2,29 +2,56 @@
 
 extern node_t *head;
 
-void delete(char * name){
+void delete (char *Name)
+{
+    node_t *temp;
+    if (head == NULL)
+    {
+        printf("\nlist is empty\n");
+    }
+    else
+    {
+        temp = head;
+        while (strcmp(temp->name, Name) != 0 && temp != NULL)
+        {
+            temp = temp->next;
+        }
+        if (temp == head && temp->next == NULL)
+        { // if only mp3 in the list
+            node_t *temp;
+            int i = 0;
 
-node_t *temp = head;
-
-while(temp!=NULL){ //we always need to traverse full list
-
-if(temp->name == name){ //if head deleted
-	head = temp->next;
-	free(temp);
-	continue;
-}
-
-while(temp->name != name && temp!=NULL){ // this goes to the next mp3 than requested
-    temp->prev=temp;
-    temp=temp->next;
-}
-if(temp->name == name){
-    temp=temp->next;
-    free(temp);
-    continue;
-}
-return;
-printf("temp name is %s",temp->name); // debug
-
-}
+            while (head != NULL)
+            {
+                temp = head;
+                head = head->next; // point to next MP3 record
+                free(temp->name);  // first free name inside MP3 record
+                free(temp);        // then free MP3 record
+                i++;
+            }
+        }
+        else if (temp == head && temp->next != NULL)
+        { // if deleting first mp3 in the list
+            head = temp->next;
+            head->prev = NULL;
+            free(temp->name);
+            free(temp);
+        }
+        else if (temp->next != NULL) // case for if you try to delete not at the end of the list
+        {
+            node_t *beforeMp3 = temp->prev; // get previous node
+            node_t *afterMp3 = temp->next;
+            beforeMp3->next = afterMp3;
+            afterMp3->prev = beforeMp3;
+            free(temp->name);
+            free(temp);
+        }
+        else // case if you delete a node at the end of the list
+        {
+            node_t *beforeMp3 = temp->prev; // get previous node
+            beforeMp3->next = NULL;
+            free(temp->name);
+            free(temp);
+        }
+    }
 }
